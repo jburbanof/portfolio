61b265beb69bdbcab76d1e9498a9437ee7164cfb
Julián Burbano <jburbano64@gmail.com>
2022-03-22 01:01:17 -0500

    Profile update

--- animations/variables.js
+++
@@ -1,6 +0,0 @@
-export let description = document.getElementById("description");
-export let scrollTop = document.documentElement.scrollTop;
-export let NavBarHeight = navBar.offsetTop;
-export let height = window.innerHeight;
-export let width = window.innerWidth;
-export let ball = document.getElementById("picture");
